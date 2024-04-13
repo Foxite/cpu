13bde158949fa5759ed1169197218dc42863aafd
Dirk Kok <the@dirkkok.nl>
2024-04-13 02:42:14 +0200

    stack machine

--- asm/proc16b/compile.sh
+++ asm/proc16b/compile.sh
@@ -4,6 +4,6 @@ dotnet run --project ../../tools/Assembler.Cli -- compile \
 	--arch proc16b \
 	--output-mode raw \
 	--macro-path macros \
-	--symbol 'STACK_PTR=$0' 'STACK_BEGIN=$0x100' \
+	--symbol 'STACK_PTR=$0' 'STACK_BEGIN=$0x100' 'FUNC_RETV=$0x1' 'FUNC_ARGS=$0x2' 'FUNC_LOCS=$0x3' \
 	--output "${1%.pa2}.out" \
 	-- "$1"
